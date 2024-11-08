5b7611d184977c0bd7ef16dded447e31e9f7a5e4
Marce Amaya <ing.orellana3@gmail.com>
2024-11-07 22:27:06 -0600

    modified js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 alert("Bienvenidos");
+
+ // JavaScript para mostrar y ocultar la imagen
+ function toggleImage() {
+    const imageContainer = document.getElementById("imageContainer");
+    if (imageContainer.style.display === "none" || imageContainer.style.display === "") {
+        imageContainer.style.display = "block"; // Muestra la imagen
+    } else {
+        imageContainer.style.display = "none"; // Oculta la imagen
+    }
+}
